f2d6fc2b812f6bb1042e49b0910764bf852db826
subin lee <cinematicmomentz@gmail.com>
2023-12-15 17:38:52 +0900

    modify: 자잘한 css 수정

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -8,11 +8,14 @@ function Hero() {
       </nav>
       <h1 className='head_text'>
         Summarize Article with <br className='max-md:hidden' />
-        <span className='orange_gradient'>OpenAI GPT-4</span>
+        <span className='orange_gradient'>OpenAI</span>
       </h1>
       <h2 className='desc'>
-        Simplify your reading with Summize, an open-source article summarizer
+        AI기반의 Summarizer를 활용하여 당신의 아티클을 간편하게 요약해 보세요.
       </h2>
+      <p className=' text-base text-gray-600 sm:text-base text-center max-w-2xl'>
+        Simplify your reading with Summize, an open-source article summarizer.
+      </p>
     </header>
   );
 }
